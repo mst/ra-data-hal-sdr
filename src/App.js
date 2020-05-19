f5b685059eb48fc00bc3784eff32ab4ba7e356e8
Marcel Steinbach <marcelst@me.com>
2020-05-19 12:30:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { render } from 'react-dom';
+import restProvider from './ra-data-hal';
+import {Create, ReferenceArrayField, ReferenceManyField, SingleFieldList, ChipField,ReferenceField, EditGuesser, SimpleForm, Edit,TextInput, List, Datagrid, TextField, DateField, Admin, Resource, ListGuesser } from 'react-admin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const HotelEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <ReferenceField label="City" source="_links.city.href" reference="cities">
+                <TextField source="country" />
+                <TextField source="name" />
+            </ReferenceField>            
+            <TextInput source="name" />
+            <TextInput source="address" />
+            <TextInput source="zip" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const CityList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <ReferenceManyField label="Hotels" source="_links.hotels.href" reference="hotels">
+                <SingleFieldList>
+                    <ChipField source="name" />
+                </SingleFieldList>
+            </ReferenceManyField>            
+            <TextField source="name" />
+            <TextField source="state" />
+            <TextField source="country" />
+            <TextField source="map" />
+       </Datagrid>
+    </List>
+);
+
+export const HotelList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <ReferenceField label="City" source="_links.city.href" reference="cities">
+              <TextField source="name" />
+            </ReferenceField>            
+            <TextField source="name" />
+            <TextField source="address" />
+            <TextField source="zip" />
+        </Datagrid>
+    </List>
+);
+
+export const UserCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput source="name" />
+        </SimpleForm>
+    </Create>
+);
+
+
+const App = () => (
+      <Admin dataProvider={restProvider('http://localhost:8080')}>
+        <Resource name="cities" list={CityList}/>
+        <Resource name="users" create={UserCreate} list={ListGuesser} edit={EditGuesser}/>
+        <Resource name="groups" create={EditGuesser} list={ListGuesser} edit={EditGuesser}/>
+        <Resource name="hotels" list={HotelList} edit={HotelEdit}/>
+    </Admin>
+)
 
 export default App;
